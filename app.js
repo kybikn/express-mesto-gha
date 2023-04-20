22186f7b77e2128aa7b275e92e7edf31c3b051a7
Kybik <82117054+kybikn@users.noreply.github.com>
2023-04-20 20:11:22 +0300

    feat: add limiter and helmet

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const router = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
@@ -9,6 +11,15 @@ const app = express();
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 500, // Limit each IP to 500 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(helmet());
+app.use(limiter);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
