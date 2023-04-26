3ef7400addd4cc31898edcf5fe57da998aafc78f
Kybik <82117054+kybikn@users.noreply.github.com>
2023-04-27 02:36:18 +0300

    feat: add auth

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
+
+const authRouter = require('./auth');
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const auth = require('../middlewares/auth');
 
-router.use('/users', usersRouter);
-router.use('/cards', cardsRouter);
-router.use((req, res) => {
+router.use(authRouter);
+router.use('/users', auth, usersRouter);
+router.use('/cards', auth, cardsRouter);
+
+router.use((req, res, next) => {
   res.status(404).json({ message: 'Not Found' });
+  next();
 });
 
 module.exports = router;
