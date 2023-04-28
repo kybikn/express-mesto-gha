37bdf4f5e7493460b026de4f47cbad42bf865199
Kybik <82117054+kybikn@users.noreply.github.com>
2023-04-28 10:42:58 +0300

    fix: add ForbiddenError

--- middlewares/checkErrors.js
+++ middlewares/checkErrors.js
@@ -8,11 +8,14 @@ const {
 } = require('../utils/constants');
 const NotFoundError = require('../errors/not-found-err');
 const UnauthorizedError = require('../errors/unauthorized-err');
+const ForbiddenError = require('../errors/forbidden-err');
 
 module.exports = (error, req, res, next) => {
   if (error.code === 11000) {
     res.status(ERROR_CONFLICT).send({ message: ERROR_CONFLICT_MESSAGE });
-  } else if (error instanceof NotFoundError || error instanceof UnauthorizedError) {
+  } else if (error instanceof NotFoundError
+    || error instanceof UnauthorizedError
+    || error instanceof ForbiddenError) {
     res.status(error.statusCode).send({ message: error.message });
   } else if (error.name === 'CastError' || error.name === 'ValidationError') {
     res.status(ERROR_BAD_REQUEST).send({ message: ERROR_BAD_REQUEST_MESSAGE });
