36b2ed0f69a8d1859f0a202bf1ba32c5d6e14a29
Kybik <82117054+kybikn@users.noreply.github.com>
2023-05-01 21:23:32 +0300

    refactor: pass errors to the central handler

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,24 @@
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const config = require('../config');
 
 const UnauthorizedError = require('../errors/unauthorized-err');
 const {
   ERROR_UNAUTHORIZED_MESSAGE,
 } = require('../utils/constants');
 
-const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = config;
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
   if (!token) {
-    throw new UnauthorizedError(ERROR_UNAUTHORIZED_MESSAGE);
+    next(new UnauthorizedError(ERROR_UNAUTHORIZED_MESSAGE));
   }
   let payload;
   const jwtSecret = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
   try {
     payload = jwt.verify(token, jwtSecret);
   } catch (err) {
-    throw new UnauthorizedError(ERROR_UNAUTHORIZED_MESSAGE);
+    next(new UnauthorizedError(ERROR_UNAUTHORIZED_MESSAGE));
   }
   req.user = payload; // записываем пейлоуд в объект запроса
   next(); // пропускаем запрос дальше
